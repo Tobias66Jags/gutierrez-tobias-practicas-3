50ed86e34f437281c2129c60bc9c4a16d844c8ab
Tobias Gutierrez <perepump@gmail.com>
2022-06-01 22:46:56 -0500

    agregando crud

---
+++ football_crud.sql
@@ -0,0 +1,66 @@
+/*Reglas de Negocio**:*/
+
+/*Players*/
+
+/*1. Crear _player_.*/
+INSERT INTO players (player_id, player_name, birthdate, photo, locker, position, jersey ) VALUES 
+  ('2018043671', 'Tobias Armando Gutierrez Salinas', "2002-10-31","https://eltobi.com/img/perfiltobi.jpg",66 ,5, 66 );
+/*1. Leer todos los _players_.*/
+SELECT * FROM players;
+/*1. Actualizar datos del _player_.*/
+
+/*1. Eliminar _player_.*/
+
+
+
+
+
+/*### Coaches*/
+
+/*1. Crear _coach_.*/
+
+/*1. Leer todos los _coach_.*/
+SELECT * FROM coaches;
+/*1. Actualizar datos del _coach_.*/
+
+/*1. Eliminar _coach_.*/
+
+
+
+
+
+/*### Jerseys*/
+
+/*1. Crear _jersey_.*/
+
+/*1. Leer todos los _jerseys_.*/
+SELECT * FROM jerseys;
+/*1. Actualizar número del _jersey_.*/
+
+/*1. Actualizar talla del _jersey_.*/
+
+
+
+
+
+/*### Lockers*/
+
+/*1. Asignar _locker_ a un _player_.*/
+
+/*. Contar el número de _lockers_ desocupados.*/
+
+/*1. Quitar _locker_ a un _player_. */
+
+
+
+
+
+/*### Position*/
+
+/*1. Crear _position_ de un _player_.*/
+
+/*1. Actualizar _position_ de un _player_.*/
+
+/*1. Leer las _positions_ de un _player_.*/
+
+/*1. Eliminar _position_ de un _player_.*/
